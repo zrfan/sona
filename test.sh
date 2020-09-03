4f200be1ea82367dc6605ebd0e4e34406c35ac29
zhongrui3 <zhongrui3@staff.weibo.com>
2020-09-03 12:50:44 +0800

    Fx

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ mvn clean install -Dmaven.test.skip=true
 #unzip ./dist/target/sona-0.1.1-bin.zip
 
 rm -r /data10/home/weibo_bigdata_push/zhongrui3/rsync/sona/sona-0.1.1-bin/lib
-mv ./dist/target/sona-0.1.1-bin/lib   /data10/home/weibo_bigdata_push/zhongrui3/rsync/sona/sona-0.1.1-bin/
+cp ./dist/target/sona-0.1.1-bin/lib   /data10/home/weibo_bigdata_push/zhongrui3/rsync/sona/sona-0.1.1-bin/
 
 
 hdfs dfs -rm -r zhongrui3/depend/sona-0.1.1-bin
